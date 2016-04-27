54876e19172947980de29174e79e0f2b8ca15a80
up637415 <up637415@myport.ac.uk>
2016-04-27 19:06:50 +0100

    Completed task 3.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-  // Hide search box on load.
+  // Search box.
   $("#siteSearchAfter").hide();
   $("#siteSearch").click(function() {
     $("#siteSearchAfter").show();
     $("#siteSearch").hide();
+  });
+  // Masonry
+  // external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+  // init Masonry after all images have loaded
+  var $grid = $('.grid').imagesLoaded( function() {
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      columnWidth: '.grid-sizer'
+    });
   });
 });
